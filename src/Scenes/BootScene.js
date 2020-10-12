4148f579f364786be9816a96683b68fef9bb5b3e
datagirl <datagirlcmr@gmail.com>
2020-10-12 18:28:55 +0100

    setting up the game

---
+++ src/Scenes/BootScene.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+import 'phaser';
+
+export default class BootScene extends Phaser.Scene {
+  constructor() {
+    super('Boot');
+  }
+
+  preload() {
+    this.load.image('logo', 'assets/zenva_logo.png');
+  }
+
+  create() {
+    this.scene.start('Preloader');
+  }
+}
