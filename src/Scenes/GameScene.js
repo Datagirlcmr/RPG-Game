66042a0bd268f03565ed7f81fd38bcf3124a43ab
datagirl <datagirlcmr@gmail.com>
2020-10-15 15:58:54 +0100

    setting up the game screne

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,19 +1,6 @@
 /* eslint-disable no-undef */
 import 'phaser';
-
-let game;
-
-const gameOptions = {
-  platformStartSpeed: 350,
-  spawnRange: [100, 350],
-  platformSizeRange: [50, 250],
-  playerGravity: 900,
-  jumpForce: 400,
-  playerStartPosition: 200,
-  jumps: 2,
-};
-
-// object containing configuration options
+import config from '../Config/config';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -22,27 +9,10 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     // load images
-    // this.load.image('logo', 'assets/logo.png');
-    this.load.image('platform', 'assets/platform.png');
-    this.load.image('player', 'assets/player.png');
+    this.load.image('logo', 'assets/logo.png');
   }
 
   create() {
-    // group with all active platforms.
-    this.platformGroup = this.add.group({
-
-      // once a platform is removed, it's added to the pool
-      removeCallback(platform) {
-        platform.platformPool.add(platform);
-      },
-    });
-
-    this.platformPool = this.add.group({
-
-      // once a platform is removed from the pool, it's added to the active platforms group
-      removeCallback(platform) {
-        platform.platformGroup.add(platform);
-      },
-    });
+    this.add.image(400, 300, 'logo');
   }
 }
