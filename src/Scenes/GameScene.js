5ce9c74a03e514275987339dc487a45a76665d5e
datagirl <datagirlcmr@gmail.com>
2020-10-19 12:39:42 +0100

    add coins to game

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable func-names */
 /* eslint-disable max-len */
 /* eslint-disable no-plusplus */
@@ -7,16 +8,39 @@ import config from '../Config/config';
 
 // global game options
 const gameOptions = {
+  // platform speed range, in pixels per second
   platformSpeedRange: [300, 300],
+
+  // spawn range, how far should be the rightmost platform from the right edge
+  // before next platform spawns, in pixels
   spawnRange: [80, 300],
+
+  // platform width range, in pixels
   platformSizeRange: [90, 300],
+
+  // a height range between rightmost platform and next platform to be spawned
   platformHeightRange: [-5, 5],
+
+  // a scale to be multiplied by platformHeightRange
   platformHeighScale: 20,
+
+  // platform max and min height, as screen height ratio
   platformVerticalLimit: [0.4, 0.8],
+
+  // player gravity
   playerGravity: 900,
+
+  // player jump force
   jumpForce: 400,
+
+  // player starting X position
   playerStartPosition: 200,
+
+  // consecutive jumps allowed
   jumps: 2,
+
+  // % of probability a coin appears on the platform
+  coinPercent: 25,
 };
 
 export default class GameScene extends Phaser.Scene {
@@ -33,11 +57,20 @@ export default class GameScene extends Phaser.Scene {
     this.load.image('bomb', 'assets/bomb.png');
     this.load.spritesheet('player',
       'assets/dude.png',
-      { frameWidth: 32, frameHeight: 48 });
+      { frameWidth: 24, frameHeight: 48 });
+    this.load.spritesheet('coin', 'assets/coin.png', {
+      frameWidth: 20,
+      frameHeight: 20,
+    });
   }
 
   create() {
+    // loading the background
     this.add.image(400, 300, 'forest');
+
+    // keeping track of added platforms
+    this.addedPlatforms = 0;
+
     // group with all active platforms.
     this.platformGroup = this.add.group({
 
@@ -47,7 +80,7 @@ export default class GameScene extends Phaser.Scene {
       },
     });
 
-    // pool
+    // platform pool
     this.platformPool = this.add.group({
 
       // once a platform is removed from the pool, it's added to the active platforms group
@@ -56,7 +89,25 @@ export default class GameScene extends Phaser.Scene {
       },
     });
 
-    // number of consecutive jumps made by the player
+    // group with all active coins.
+    this.coinGroup = this.add.group({
+
+      // once a coin is removed, it's added to the pool
+      removeCallback(coin) {
+        coin.scene.coinPool.add(coin);
+      },
+    });
+
+    // coin pool
+    this.coinPool = this.add.group({
+
+      // once a coin is removed from the pool, it's added to the active coins group
+      removeCallback(coin) {
+        coin.scene.coinGroup.add(coin);
+      },
+    });
+
+    // number of consecutive jumps made by the player so far
     this.playerJumps = 0;
 
     // adding a platform to the game, the arguments are platform width, x position and y position
@@ -77,6 +128,18 @@ export default class GameScene extends Phaser.Scene {
       repeat: -1,
     });
 
+    // setting coin animation
+    this.anims.create({
+      key: 'rotate',
+      frames: this.anims.generateFrameNumbers('coin', {
+        start: 0,
+        end: 5,
+      }),
+      frameRate: 15,
+      yoyo: true,
+      repeat: -1,
+    });
+
     // setting collisions between the player and the platform group
     this.physics.add.collider(this.player, this.platformGroup, function () {
       // play "run" animation if the player is on a platform
@@ -85,28 +148,69 @@ export default class GameScene extends Phaser.Scene {
       }
     }, null, this);
 
+    // setting collisions between the player and the coin group
+    this.physics.add.overlap(this.player, this.coinGroup, function (player, coin) {
+      this.tweens.add({
+        targets: coin,
+        y: coin.y - 100,
+        alpha: 0,
+        duration: 800,
+        ease: 'Cubic.easeOut',
+        callbackScope: this,
+        onComplete() {
+          this.coinGroup.killAndHide(coin);
+          this.coinGroup.remove(coin);
+        },
+      });
+    }, null, this);
+
     // checking for input
     this.input.on('pointerdown', this.jump, this);
   }
 
   // the core of the script: platform are added from the pool or created on the fly
   addPlatform(platformWidth, posX, posY) {
+    this.addedPlatforms++;
     let platform;
     if (this.platformPool.getLength()) {
       platform = this.platformPool.getFirst();
       platform.x = posX;
+      platform.y = posY;
       platform.active = true;
       platform.visible = true;
       this.platformPool.remove(platform);
+      // const newRatio = platformWidth / platform.displayWidth;
+      platform.displayWidth = platformWidth;
+      platform.tileScaleX = 1 / platform.scaleX;
     } else {
-      platform = this.physics.add.sprite(posX, posY, 'platform');
-      platform.setImmovable(true);
-      platform.setVelocityX(Phaser.Math.Between(gameOptions.platformSpeedRange[0], gameOptions.platformSpeedRange[1]) * -1);
-      // platform.setVelocityX(gameOptions.platformStartSpeed * -1);
+      platform = this.add.tileSprite(posX, posY, platformWidth, 22, 'platform');
+      this.physics.add.existing(platform);
+      platform.body.setImmovable(true);
+      platform.body.setVelocityX(Phaser.Math.Between(gameOptions.platformSpeedRange[0], gameOptions.platformSpeedRange[1]) * -1);
       this.platformGroup.add(platform);
     }
-    platform.displayWidth = platformWidth;
     this.nextPlatformDistance = Phaser.Math.Between(gameOptions.spawnRange[0], gameOptions.spawnRange[1]);
+
+    // is there a coin over the platform?
+    if (this.addedPlatforms > 1) {
+      if (Phaser.Math.Between(1, 100) <= gameOptions.coinPercent) {
+        if (this.coinPool.getLength()) {
+          const coin = this.coinPool.getFirst();
+          coin.x = posX;
+          coin.y = posY - 96;
+          coin.alpha = 1;
+          coin.active = true;
+          coin.visible = true;
+          this.coinPool.remove(coin);
+        } else {
+          const coin = this.physics.add.sprite(posX, posY - 66, 'coin');
+          coin.setImmovable(true);
+          coin.setVelocityX(platform.body.velocity.x);
+          coin.anims.play('rotate');
+          this.coinGroup.add(coin);
+        }
+      }
+    }
   }
 
   // the player jumps when on the ground, or once in the air as long as there are jumps left and the first jump was on the ground
@@ -144,11 +248,18 @@ export default class GameScene extends Phaser.Scene {
       }
     }, this);
 
+    // recycling coins
+    this.coinGroup.getChildren().forEach(function (coin) {
+      if (coin.x < -coin.displayWidth / 2) {
+        this.coinGroup.killAndHide(coin);
+        this.coinGroup.remove(coin);
+      }
+    }, this);
+
     // adding new platforms
     if (minDistance > this.nextPlatformDistance) {
       const nextPlatformWidth = Phaser.Math.Between(gameOptions.platformSizeRange[0], gameOptions.platformSizeRange[1]);
       const platformRandomHeight = gameOptions.platformHeighScale * Phaser.Math.Between(gameOptions.platformHeightRange[0], gameOptions.platformHeightRange[1]);
-      // console.log(rightmostPlatformHeight);
       const nextPlatformGap = rightmostPlatformHeight + platformRandomHeight;
       const minPlatformHeight = config.height * gameOptions.platformVerticalLimit[0];
       const maxPlatformHeight = config.height * gameOptions.platformVerticalLimit[1];
