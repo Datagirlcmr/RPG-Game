8711a575056859fe43fe71357c9de57105156d24
datagirl <datagirlcmr@gmail.com>
2020-10-26 19:53:05 +0100

    mute webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,57 @@
-const webpack = require('webpack');
-const path = require('path');
+// const webpack = require('webpack');
+// const path = require('path');
 
-module.exports = {
+// module.exports = {
 
-  entry: './src/index.js',
+//   entry: './src/index.js',
 
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/build/',
-    filename: 'project.bundle.js',
-  },
+//   output: {
+//     path: path.resolve(__dirname, 'build'),
+//     publicPath: '/build/',
+//     filename: 'project.bundle.js',
+//   },
 
-  module: {
-    rules: [
-      {
-        test: [/\.vert$/, /\.frag$/],
-        use: 'raw-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|jpe?g|gif|mp3|ogg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        // include: path.resolve(__dirname, 'src/'),
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/env'],
-          },
-        },
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
+//   module: {
+//     rules: [
+//       {
+//         test: [/\.vert$/, /\.frag$/],
+//         use: 'raw-loader',
+//       },
+//       {
+//         test: /\.(png|jpe?g|gif|mp3|ogg)$/i,
+//         use: [
+//           {
+//             loader: 'file-loader',
+//           },
+//         ],
+//       },
+//       {
+//         test: /\.js$/,
+//         // include: path.resolve(__dirname, 'src/'),
+//         exclude: /node_modules/,
+//         use: {
+//           loader: 'babel-loader',
+//           options: {
+//             presets: ['@babel/env'],
+//           },
+//         },
+//       },
+//       {
+//         test: /\.html$/i,
+//         loader: 'html-loader',
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
 
-  plugins: [
-    new webpack.DefinePlugin({
-      CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true),
-    }),
-  ],
+//   plugins: [
+//     new webpack.DefinePlugin({
+//       CANVAS_RENDERER: JSON.stringify(true),
+//       WEBGL_RENDERER: JSON.stringify(true),
+//     }),
+//   ],
 
-};
+// };
