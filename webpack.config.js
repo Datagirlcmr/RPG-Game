f76cf7f8995ec251c3717e15ec4a6dd4034f12f0
datagirl <datagirlcmr@gmail.com>
2020-10-26 19:22:53 +0100

    add a css loader

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,10 @@ module.exports = {
         use: 'raw-loader',
       },
       {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.(png|jpe?g|gif|mp3|ogg)$/i,
         use: [
           {
