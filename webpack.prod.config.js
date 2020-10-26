7367e84367bcfec4aa356e72c3f53663222a3fe1
datagirl <datagirlcmr@gmail.com>
2020-10-26 22:24:20 +0100

    debugging

---
+++ webpack.prod.config.js
@@ -0,0 +1,20 @@
+const webpack = require('webpack');
+const config = require('./webpack.config.js');
+
+config.plugins.push(
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production'),
+    },
+  }),
+);
+
+config.plugins.push(
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+  }),
+);
+
+module.exports = config;
